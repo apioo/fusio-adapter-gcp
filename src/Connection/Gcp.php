f1be61c7fecc994a9c55f67894f9edd7c5b47945
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-07-15 19:38:32 +0200

    update

--- src/Connection/Gcp.php
+++ src/Connection/Gcp.php
@@ -31,7 +31,7 @@ use Google\Cloud\Core\ServiceBuilder;
  * Gcp
  *
  * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
- * @license http://www.gnu.org/licenses/agpl-3.0
+ * @license http://www.apache.org/licenses/LICENSE-2.0
  * @link    https://www.fusio-project.org/
  */
 class Gcp extends ConnectionAbstract
--- tests/Connection/GcpTest.php
+++ tests/Connection/GcpTest.php
@@ -34,7 +34,7 @@ use Google\Cloud\Core\ServiceBuilder;
  * GcpTest
  *
  * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
- * @license http://www.gnu.org/licenses/agpl-3.0
+ * @license http://www.apache.org/licenses/LICENSE-2.0
  * @link    https://www.fusio-project.org/
  */
 class GcpTest extends GcpTestCase
